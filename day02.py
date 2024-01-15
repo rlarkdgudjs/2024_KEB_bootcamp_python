1dc088a541347d65cfc3cab7eb82466dde7fa3cf
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-15 13:26:13 +0900

    v0.11

--- day02.py
+++
@@ -1,8 +0,0 @@
-base = int(input('base : '))
-exponent = int(input('exp : '))
-# f-string
-# print(f'밑은 {base}, 시수는 {exponent}, 결과값은 {base**exponent}')
-print(f'밑은 {base}, 시수는 {exponent}, 결과값은 {pow(base, exponent)}')
-
-# format function
-print('밑은 {0}, 시수는 {1}, 결과값은 {2}'.format(base, exponent, base**exponent))
