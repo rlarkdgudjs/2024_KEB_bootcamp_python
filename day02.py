cf24bc9849b03b0a41338134e8b26a0c688ce1ee
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-15 15:47:09 +0900

    v0.6

--- day02.py
+++ day02.py
@@ -1,11 +1,7 @@
-menu = input("1) ºF -> ºC   2) ºC -> ºF   3) Quit ")
-if menu == '1':
-    Fahrenheit = float(input('INPUT Fahrenheit : '))
-    Calculate_Celsius = (Fahrenheit - 32) * 5 / 9
-    print(f'화씨 : {Fahrenheit}ºF 섭씨 : {Calculate_Celsius:.4f}ºC')
-elif menu == '2':
-    Celsius = float(input('INPUT Celsius : '))
-    Calculate_Fahrenheit = (Celsius * 9 / 5) + 32
-    print(f'섭씨 : {Celsius}ºC 화씨 : {Calculate_Fahrenheit:.4f}ºF')
+letter = input('INPUT alphabet : ')
+# vowels = {'a','e','i','o','u'} #set
+vowels = 'aeiou'
+if letter in vowels:
+    print(f'{letter} is a vowel')
 else:
-    print("종료")
+    print(f'{letter} is a consonant')
