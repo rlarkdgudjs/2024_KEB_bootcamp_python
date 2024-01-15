8d9c419b488301f92299ff2a2758a987942a2d61
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-15 16:13:11 +0900

    day02HW

---
+++ day02HW.py
@@ -0,0 +1,12 @@
+menu = input("1) ºF -> ºC   2) ºC -> ºF   3) Quit ")
+while menu != '3':
+    if menu == '1':
+        Fahrenheit = float(input('INPUT Fahrenheit : '))
+        Calculate_Celsius = (Fahrenheit - 32) * 5 / 9
+        print(f'화씨 : {Fahrenheit}ºF 섭씨 : {Calculate_Celsius:.4f}ºC')
+    elif menu == '2':
+        Celsius = float(input('INPUT Celsius : '))
+        Calculate_Fahrenheit = (Celsius * 9 / 5) + 32
+        print(f'섭씨 : {Celsius}ºC 화씨 : {Calculate_Fahrenheit:.4f}ºF')
+    menu = input("1) ºF -> ºC   2) ºC -> ºF   3) Quit ")
+print("종료")
