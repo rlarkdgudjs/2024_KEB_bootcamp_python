d4105ea22117a9514aaadedc151150deef0153d7
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-15 15:38:34 +0900

    v0.51

--- day02.py
+++ day02.py
@@ -1,9 +1,11 @@
-menu = input("1) F -> C   2) C -> F   3) Quit ")
+menu = input("1) ºF -> ºC   2) ºC -> ºF   3) Quit ")
 if menu == '1':
     Fahrenheit = float(input('INPUT Fahrenheit : '))
     Calculate_Celsius = (Fahrenheit - 32) * 5 / 9
-    print(f'화씨 : {Fahrenheit}F 섭씨 : {Calculate_Celsius:.4f}C')
+    print(f'화씨 : {Fahrenheit}ºF 섭씨 : {Calculate_Celsius:.4f}ºC')
 elif menu == '2':
     Celsius = float(input('INPUT Celsius : '))
     Calculate_Fahrenheit = (Celsius * 9 / 5) + 32
-    print(f'섭씨 : {Celsius}C 화씨 : {Calculate_Fahrenheit:.4f}F')
+    print(f'섭씨 : {Celsius}ºC 화씨 : {Calculate_Fahrenheit:.4f}ºF')
+else:
+    print("종료")
