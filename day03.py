4a8e31c17f5887230499b2567917273ba8b220cf
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-16 11:44:36 +0900

    v0.91

--- day03.py
+++ day03.py
@@ -1,6 +1,8 @@
 subjects = "python c++ database linux"
 subject = input("수강신청과목 입력 : ")
-if subjects.find(subject) != -1:
-    print(f"해당 과목은 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
-else:
+
+try:
+    print(f"해당 과목은 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
     print(f'해당 과목은 존재하지 않습니다.')
+
