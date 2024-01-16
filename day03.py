b7469f2fcea4d75d3b84020ce0f86b4238914d73
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-16 14:59:51 +0900

    v2.4

--- day03.py
+++ day03.py
@@ -1,18 +1,15 @@
-# prime number
-number = int(input("INPUT number : "))
-is_prime = True
-i = 2
-if number < 2:
-    print(f'{number} is not prime number')
-else:
-    while i < number:
-        if number % i == 0:
-            is_prime = False
-            break
-        i += 1
+univ = "inha"
+i = 0
+while i < len(univ):
+    print(univ[i], end='')
+    i += 1
 
-    if is_prime:
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is not prime number')
+print()
 
+for j in univ:
+    print(j, end='')
+
+print()
+
+for k in range(len(univ)):
+    print(univ[k], end='')
