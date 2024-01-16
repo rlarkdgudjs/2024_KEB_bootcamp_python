d380c92f2e569acc33328467ea1af92b33ba093a
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-16 11:34:25 +0900

    v0.8

--- day03.py
+++ day03.py
@@ -1,8 +1,18 @@
-# univ = "Inha\nUnv."
-# # univ = r"Inha\nUnv." #raw string
-# print(univ)
-# number1 = input("First number : ")
-# number2 = input("Second number : ")
-# print(number1,number2) #concatenaton
+course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
+print(course.find('KEB'))
+print(course.rfind('KEB'))
+print(course.index('KEB'))
 
 
+# subjects = ["python", "c++", "database"]
+# subjects_string = " ".join(subjects)
+# print(subjects_string)
+
+# numbers = input("FirstNumber SecodNumber : ").split()
+# # print(numbers[0] + numbers[1]) # concatenation
+# print(int(numbers[0]) + int(numbers[1])) # arithmetic operation
+
+# course = "2024 KEB Bootcamp"
+# print(course)
+# list_course = course.split('B')
+# print(list_course)
