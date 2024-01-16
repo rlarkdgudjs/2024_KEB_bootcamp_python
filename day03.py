89353e26dd41875dafc845c314dea929afa2ab78
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-16 10:34:47 +0900

    v.0.7

---
+++ day03.py
@@ -0,0 +1,8 @@
+# univ = "Inha\nUnv."
+# # univ = r"Inha\nUnv." #raw string
+# print(univ)
+# number1 = input("First number : ")
+# number2 = input("Second number : ")
+# print(number1,number2) #concatenaton
+
+
