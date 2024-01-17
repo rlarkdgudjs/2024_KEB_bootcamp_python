908f4fbff65e2ecb421e0aa7e231ddc3d3bb547c
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-17 14:43:11 +0900

    v4.6

--- day04.py
+++ day04.py
@@ -1,5 +1,25 @@
-# squares = [i*i for i in range(1, 6)]  # list comprehension
-# print(squares)
+sugang = dict(python="kim", cpp="sung", db="kang")
+# print(sugang)
+# sugang['datastructure'] = 'kim'
+# print(sugang)
+# sugang['datastructure'] = 'park'
+# print(sugang)
+# print(sugang['db'])
+# print(sugang.get('db'))
+# print(sugang.get('opensource', '존재하지 않습니다'))
 
-even_squares = [i*i for i in range(1, 6) if i%2==0]
-print(even_squares)
+
+for subject, professor in sugang.items():
+    print(f'{subject} 과목 담당교수는 {professor}')
+
+for k in sugang:
+    print(k)
+
+for k in sugang.keys():
+    print(k)
+
+for v in sugang.values():
+    print(v)
+
+for v in sugang.items():
+    print(v)
