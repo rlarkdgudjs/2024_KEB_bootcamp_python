9b416f9d8292eb1fad50ddcc62446cf2c47da96f
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-17 10:35:24 +0900

    v4.2

--- day04.py
+++ day04.py
@@ -1,27 +1,10 @@
-# prime number
-start, finish = map(int, input("INPUT 'start finish' : ").split())
-
-if start > finish:
-    start, finish = finish, start
-
-for number in range(start, finish + 1):
-    is_prime = True
-
-    if number < 2:
-        continue
-    else:
-        i = 2
-        while i*i <= number:
-            if number % i == 0:
-                is_prime = False
-                break
-            i += 1
-            # print(i, end=' ')
-        # for j in range(2, number):
-        #     if number % j == 0:
-        #         is_prime = False
-        #         break
-        if is_prime:
-            # pass
-            print(number, end=' ')
-
+t1 = 5, "no.1"  # packing
+print(type(t1))
+print(type(t1[1]))
+print(type(t1[0]))
+ea, num = t1
+print(ea, num)
+t2 = 1,3,5
+t3 = 1,
+print(t2==t3)
+print(t2 + t3)
