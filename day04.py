df5aa2885c07738ca1e288be841763f6e8a5b7f0
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-17 13:33:52 +0900

    v4.4

--- day04.py
+++ day04.py
@@ -1,9 +1,15 @@
-subjects = ["C++", "java", "python"]
-print(subjects[::-1])
-subjects.reverse()
-print(subjects)
-del subjects[-1]
-print(subjects)
-a = subjects.pop()
-print(a)
-print(subjects)
+import copy
+# subjects = ["a", "b", "13"]
+subjects = ["a", ["b", "13"], "d"]
+a = subjects
+b = subjects.copy()
+c = list(a)
+d = subjects[:]
+e = copy.deepcopy(subjects)
+
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e) # 리스트 원소중에 mutable 한값이 바뀌면 shallow copy한 값들도 같이 바뀜
+# pythontutor.com
+
+
