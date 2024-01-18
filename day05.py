5fdcd0cfb7a5a3d9ff22b94fb1cb2cef16737b3a
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-18 10:52:00 +0900

    v5.Primenumber

--- day05.py
+++ day05.py
@@ -1,9 +1,25 @@
-# univ = 'inha university'
-# count_letter = {letter: univ.count(letter) for letter in univ} # dictionary comprehension
-# print(count_letter)
+# prime number
+def isprime(n) -> bool:
+    """
+    매개변수로 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
 
-univ = 'inha university'
-count_letter = dict()
-for letter in univ:
-    count_letter[letter] = univ.count(letter)
-print(count_letter)
+
+start, finish = map(int, input("INPUT 'start finish' : ").split())
+
+if start > finish:
+    start, finish = finish, start
+for number in range(start, finish + 1):
+    if isprime(number):
+        print(number, end=' ')
