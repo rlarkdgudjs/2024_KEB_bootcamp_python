9004b460edd9db4abb9705d2c5ac45b7188f920c
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-19 15:10:09 +0900

    v6.6

--- day06.py
+++ day06.py
@@ -1,36 +1,28 @@
+class FlyingMixin:
+    def fly(self):
+        return f'{self.name}이(가) 비행중입니다.'
+
+
+class SwimmingMixin:
+    def swim(self):
+        return f'{self.name}이(가) 수영중입니다.'
+
+
 class Pokemon:
-    def __init__(self, name):
+    def __init__(self,name):
         self.name = name
 
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격!')
 
-
-class Pikachu(Pokemon):  # is-a
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 향해 전광석화!')
-
-    def type_info(self):
-        print(f"{self.type} 타입 포켓몬")
-
-
-class Squirtle(Pokemon):
+class Charizard(Pokemon, FlyingMixin):
     pass
 
 
-class Agumon:
+class Gyarados(Pokemon, SwimmingMixin, FlyingMixin):
     pass
 
+g1 = Gyarados("갸랴도스")
+c1 = Charizard("리자몽")
 
-p1 = Pikachu("피카츄", "전기")
-p2 = Squirtle('꼬부기')
-p1.attack(p2)
-p2.attack(p1)
-p1.type_info()
-print(p1.name, p1.type)
-print(issubclass(Pikachu, Pokemon))
-print(issubclass(Agumon, Pokemon))
+print(g1.fly())
+print(g1.swim())
+print(c1.fly())
