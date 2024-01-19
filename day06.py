a82b49a57002d4fb3f9292758022a79f7dcb06cb
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-19 10:54:35 +0900

    v6.1

--- day06.py
+++ day06.py
@@ -1,26 +1,14 @@
-def factorial_repetition(n):
-    """
-    반복문을 이용한 팩토리얼 함수
-    :param n: 정수
-    :return: 팩토리얼 값
-    """
-    result = 1
-    for i in range(2, n+1):
-        result *= i
-    return result
+import random
 
+numbers = [random.randint(1, 100) for i in range(random.randint(1, 10))]
 
-def factorial_recursion(n):
-    """
-    재귀함수를 이용한 팩토리얼 함수
-    :param n: 정수
-    :return: function, int
-    """
-    if n == 1:
-        return n
-    else:
-        return n*factorial_repetition(n-1)
-
-
-print(factorial_repetition(int(input("number : "))))
-print(factorial_recursion(int(input("number : "))))
+print(numbers)
+try:
+    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
+    print(numbers[pick])
+except IndexError as err:
+    print(f"{err} : Wrong index number ")
+except ValueError as err:
+    print(f"{err} : Input only number ")
+except Exception:
+    print("Error occurs")
