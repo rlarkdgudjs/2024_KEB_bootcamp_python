d743d453e9065e16dc758726690033cf0c99a96a
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-19 15:17:50 +0900

    v6.61

--- day06.py
+++ day06.py
@@ -2,27 +2,29 @@ class FlyingMixin:
     def fly(self):
         return f'{self.name}이(가) 비행중입니다.'
 
-
 class SwimmingMixin:
     def swim(self):
         return f'{self.name}이(가) 수영중입니다.'
-
 
 class Pokemon:
     def __init__(self,name):
         self.name = name
 
+    def attack(self):
+        print(f'{self.name}이(가) 공격!')
 
 class Charizard(Pokemon, FlyingMixin):
     pass
-
 
 class Gyarados(Pokemon, SwimmingMixin, FlyingMixin):
     pass
 
 g1 = Gyarados("갸랴도스")
 c1 = Charizard("리자몽")
-
-print(g1.fly())
-print(g1.swim())
-print(c1.fly())
+# print(g1.fly())
+# print(g1.swim())
+# print(c1.fly())
+# Charizard.attack(c1)
+print(g1.name)
+g1.name = '잉어킹'
+print(g1.name)
