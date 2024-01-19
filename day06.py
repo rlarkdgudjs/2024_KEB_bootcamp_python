3c496ff1ff5ddf43eaf09e2ab99533ac6f2e72d6
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-19 11:32:08 +0900

    v6.3

--- day06.py
+++ day06.py
@@ -1,21 +1,14 @@
-import random
+def desc(func):
+    def wrap():
+        print("w")
+        func()
+        print("w")
+    return wrap
 
-class OopsException(Exception):
-    pass
-numbers = [random.randint(1, 100) for i in range(random.randint(1, 10))]
 
-print(numbers)
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-    raise OopsException("Oops!!")
-except IndexError as err:
-    print(f"{err} : Wrong index number ")
-except ValueError as err:
-    print(f"{err} : Input only number ")
-except OopsException as err:
-    print(f'Oops {err}')
-except Exception:
-    print("Error occurs")
-else:
-    print(f"Program Terminate")
+def some():
+    print("do some")
+
+
+Play = desc(some)
+Play()
