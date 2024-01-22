a378bfce98bc5a515822df3f929823ad322eaf1c
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-22 11:43:36 +0900

    v7.7

--- day07.py
+++ day07.py
@@ -1,23 +1,33 @@
-class FlyingBehavior():
-    def fly(self):
-        return f'{self.name}이(가) 공중날기를 사용합니다.'
-class JetPack(FlyingBehavior):
-    def fly(self):
-        return f'제트팩을 사용합니다.'
-
-class NoFly(FlyingBehavior):
-    def fly(self):
-        return f'공중날기를 사용할 수 없습니다.'
-class FlywWing(FlyingBehavior):
-    def fly(self):
-        return f'공중날기를 사용합니다'
-
-class Pikachu:
-    def __init__(self, name, Hp):
-        self.name = name
-        self.Hp = Hp
-        self.fly_behavior = NoFly()  # composition
+import mymath
 
 
-p1 = Pikachu("피카츄",35)
-print(p1.fly_behavior.fly())
+while True:
+    menu = input("1) ºF -> ºC   2) ºC -> ºF   3) 소수 판별하기 4)구간별 소수 구하기 5) 종료 ")
+    if menu == '1':
+        Fahrenheit = float(input('INPUT Fahrenheit : '))
+        Calculate_Celsius = (Fahrenheit - 32) * 5 / 9
+        print(f'화씨 : {Fahrenheit}ºF 섭씨 : {Calculate_Celsius:.4f}ºC')
+    elif menu == '2':
+        Celsius = float(input('INPUT Celsius : '))
+        Calculate_Fahrenheit = (Celsius * 9 / 5) + 32
+        print(f'섭씨 : {Celsius}ºC 화씨 : {Calculate_Fahrenheit:.4f}ºF')
+    elif menu == '3':
+        num_3 = int(input("INPUT number : "))
+        if mymath.isprime(num_3):
+            print(f'{num_3}는 소수 입니다.')
+        else:
+            print(f'{num_3}는 소수가 아닙니다')
+    elif menu == '4':
+        start, finish = map(int, input("INPUT 'start finish' : ").split())
+
+        if start > finish:
+            start, finish = finish, start
+        for number in range(start, finish + 1):
+            if mymath.isprime(number):
+                 print(number, end=' ')
+        print('\n')
+    elif menu == "5":
+        print("종료")
+        break
+    else:
+        print("INVALID MENU")
