859a6df675ee27bb2b2f3c033a7fff6ddb2a8663
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-01-22 11:38:19 +0900

    v7.6

--- day07.py
+++ day07.py
@@ -1,6 +1,6 @@
 class FlyingBehavior():
     def fly(self):
-        return f'{self.__name}이(가) 공중날기를 사용합니다.'
+        return f'{self.name}이(가) 공중날기를 사용합니다.'
 class JetPack(FlyingBehavior):
     def fly(self):
         return f'제트팩을 사용합니다.'
@@ -13,14 +13,11 @@ class FlywWing(FlyingBehavior):
         return f'공중날기를 사용합니다'
 
 class Pikachu:
-    def __init__(self, name ,Hp, fly):
-        self.name=name
-        self.Hp=Hp
-        self.fly_behavior=fly # aggregation
+    def __init__(self, name, Hp):
+        self.name = name
+        self.Hp = Hp
+        self.fly_behavior = NoFly()  # composition
 
 
-
-nofly = NoFly()
-wings = FlywWing()
-p1 = Pikachu("피카츄",35,nofly)
+p1 = Pikachu("피카츄",35)
 print(p1.fly_behavior.fly())
